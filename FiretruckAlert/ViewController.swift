703749a80cc93e05746cf4305996b824be57f848
iportuga <iportuga@v1020-wn-196-90.campus-dynamic.uwaterloo.ca>
2018-06-02 16:38:51 -0400

    background mode

--- FiretruckAlert/ViewController.swift
+++ FiretruckAlert/ViewController.swift
@@ -7,32 +7,73 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
     
     //MARK: Properties
+    let locationManager = CLLocationManager()
     
     @IBOutlet weak var requestTextField: UITextField!
-    @IBOutlet weak var responseTextField: UITextField!
     @IBOutlet weak var sendButton: UIButton!
     
-    let URL_WEB_SERVER = "http://192.168.56.101:8000/api/trucks"
+    let URL_WEB_SERVER = "http://192.168.56.101:8000/api/cars/createCar"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Ask for authorization.
+        self.locationManager.requestAlwaysAuthorization()
+        
+        // Allows background location updates.
+        locationManager.allowsBackgroundLocationUpdates = true
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
-    //MARK: Actions
     
-    // Sends information to the web server
-    @IBAction func sendRequest(_ sender: UIButton) {
+    
+    //MARK: CLLocationManagerDelegate methods
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        // Captures coordination points
+        if let location: CLLocation = manager.location {
+            print("locations = \(location.coordinate.latitude) \(location.coordinate.longitude)")
+            
+            // Converts location to road name.
+            lookUpCurrentLocation(location: manager.location!, completionHandler: sendPostDataToServer)
+
+        }
+    }
+    
+    func lookUpCurrentLocation(location: CLLocation, completionHandler: @escaping (String) -> Void) {
+        let geocoder = CLGeocoder()
+        
+        // Look up the location and pass it to the completion handler
+        geocoder.reverseGeocodeLocation(location, completionHandler: {(placemarks, error) in
+            if error == nil {
+                if let street = placemarks?[0].thoroughfare {
+                    completionHandler(street)
+                }
+            }
+        })
+        
+    }
+    
+    
+    
+    //MARK: Private methods
+    // Sends data to the webserver
+    func sendPostDataToServer(street: String) {
         
         // create NSURL
         let requestURL = URL(string: URL_WEB_SERVER)
@@ -41,17 +82,14 @@ class ViewController: UIViewController {
         var request = URLRequest(url: requestURL!)
         
         // set the method to POST
-        request.httpMethod = "GET"
-        
-        // get values from textField
-        let letter = requestTextField.text!
+        request.httpMethod = "POST"
         
         // set the POST parameters
-        //let postParameters = "letter="+letter
-        let postParameters = "name=Ivens&email=ivensportugal&previewAccess=yes&theme=UWaterloo"
+        let postParameters = "street="+street
+        
         
         // set parameters to body
-        //request.httpBody = postParameters.data(using: String.Encoding.utf8)
+        request.httpBody = postParameters.data(using: String.Encoding.utf8)
         
         // create a task to send POST requests
         let task = URLSession.shared.dataTask(with: request) {
@@ -62,33 +100,49 @@ class ViewController: UIViewController {
                 return
             }
             
-            // parse the response
-            do {
-                
-                // convert response to NSDictionary
-                let myJSON = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as? NSDictionary
-                
-                // parse the JSON
-                if let parseJSON = myJSON {
-                    
-                    var response: String!
-                    //response = parseJSON["capitalizedLetter"] as! String?
-                    
-                    print("answer")
-                    print(parseJSON)
-                    /*
-                    // add response to textfield
-                    DispatchQueue.main.async {
-                        self.responseTextField.text = response
-                    }*/
-                }
-            } catch {
-                print(error)
-            }
+            /*
+             // parse the response
+             do {
+             
+             // convert response to NSDictionary
+             let myJSON = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as? NSDictionary
+             
+             // parse the JSON
+             if let parseJSON = myJSON {
+             
+             var response: String!
+             //response = parseJSON["capitalizedLetter"] as! String?
+             
+             print("answer")
+             print(parseJSON)
+             /*
+             // add response to textfield
+             DispatchQueue.main.async {
+             self.responseTextField.text = response
+             }*/
+             }
+             } catch {
+             print(error)
+             }
+             */
         }
         
         // execute the task
         task.resume()
+    }
+    
+    
+
+
+    //MARK: Actions
+    
+    // Starts communication with the web server
+    @IBAction func sendRequest(_ sender: UIButton) {
+        
+        // get values from textField
+        let street = requestTextField.text!
+        self.sendPostDataToServer(street: street)
+        self.requestTextField.text = ""
     }
 }
 
